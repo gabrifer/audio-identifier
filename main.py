f28e1257ff92970e53c75cc6a4afc0a2e336b8b8
Gabriel <gabrifer@gmail.com>
2022-10-26 15:33:10 -0300

    Moving logic to main script

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import audioextractor
 import audiosplitter
+import featureextractorengine
 
 audioextractor.extract_audios()
 
 audiosplitter.split_audios()
+
+featureextractorengine.extract_features()
